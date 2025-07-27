baf66ed77796eac45e5cb4f3daa01528c0f081ed
alwannelson <alwanaditiaduha@gmail.com>
2025-07-28 01:11:05 +0700

    tampilan form login fixed

--- js/auth.js
+++ js/auth.js
@@ -1,11 +1,21 @@
 const container = document.querySelector('.container');
 const registerBtn = document.querySelector('.register-btn');
 const loginBtn = document.querySelector('.login-btn');
+const loginSubmit = document.getElementById('loginSubmit');
+let registerSubmit = document.getElementById('registerSubmit');
 
 registerBtn.addEventListener('click', () => {
-    container.classList.add('active');
+  container.classList.add('active');
 })
 
 loginBtn.addEventListener('click', () => {
-    container.classList.remove('active');
+  container.classList.remove('active');
+})
+
+loginSubmit.addEventListener('click', () => {
+  window.location.href = "saya/user.html"
+})
+
+registerSubmit.addEventListener('click', () => {
+  window.location.href = "saya/user.html"
 })
